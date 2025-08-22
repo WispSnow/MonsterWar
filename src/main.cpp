4f39afaa1314257b73dec7666dc728e43ba97bfc
Ziyu <theorhythm@gmail.com>
2025-08-22 11:24:46 +0800

    view

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <spdlog/spdlog.h>
 #include <entt/entt.hpp>
 
-// 组件是一些简单的结构体或类，它们只包含数据。
-// 定义两个组件：位置和速度。
 struct position {
     float x;
     float y;
@@ -13,45 +11,52 @@ struct velocity {
     float dy;
 };
 
+// 增加一个新组件，用于标记静态物体，帮助我们演示视图的过滤能力。
+struct immovable_tag {};
+
 int main() {
-    // 1. 创建一个 Registry
-    // Registry 是所有实体和组件的容器，可以看作是你的“游戏世界”。
     entt::registry registry;
 
-    // 2. 创建实体
-    // 实体本身只是一个唯一的标识符 (ID)。
     entt::entity player = registry.create();
-    entt::entity enemy = registry.create();
-
-    // 3. 向实体添加组件
-    // 使用 emplace<ComponentType>(entity, args...) 来添加并初始化一个组件。
     registry.emplace<position>(player, 10.f, 20.f);
-    registry.emplace<velocity>(player, 1.f, 0.f);
+    registry.emplace<velocity>(player, 1.f, 0.5f);
 
-    registry.emplace<position>(enemy, 100.f, 50.f);     // 敌人暂时没有速度组件
-
-    // 4. 修改组件
-    // 获取组件的引用后，可以直接修改它。
-    auto& player_pos = registry.get<position>(player);
-    spdlog::info("玩家位置: ({}, {})", player_pos.x, player_pos.y);
-    player_pos.x += 5.f;
-    spdlog::info("玩家位置: ({}, {})", player_pos.x, player_pos.y);
-
-    // 5. 移除组件
-    // 使用 remove<ComponentType>(entity) 来移除一个组件。
-    registry.remove<velocity>(player);
-
-    // 6. 销毁实体
-    // 销毁实体会自动移除它拥有的所有组件。
-    registry.destroy(enemy);
+    entt::entity enemy = registry.create();
+    registry.emplace<position>(enemy, 100.f, 50.f);
+    registry.emplace<velocity>(enemy, -0.5f, -1.f);
     
-    // 检查一个实体是否还存在
-    if (registry.valid(player)) {
-        spdlog::info("玩家实体仍然有效。");
+    entt::entity obstacle = registry.create();
+    registry.emplace<position>(obstacle, 200.f, 200.f);
+    registry.emplace<immovable_tag>(obstacle); // 障碍物有位置，但没有速度，并且不可移动
+
+    spdlog::info("=== 游戏update更新前 ===");
+    auto initial_player_pos = registry.get<position>(player);
+    spdlog::info("玩家初始位置: ({}, {})", initial_player_pos.x, initial_player_pos.y);
+    auto initial_enemy_pos = registry.get<position>(enemy);
+    spdlog::info("敌人初始位置: ({}, {})", initial_enemy_pos.x, initial_enemy_pos.y);
+
+
+    // === 游戏更新 （通常在System中进行）===
+    // 1. 创建一个视图
+    // 这个视图会包含所有 *同时* 拥有 position 和 velocity 组件的实体。
+    // 障碍物 (obstacle) 不会被包含在内，因为它没有 velocity 组件。
+    auto view = registry.view<position, velocity>();
+    
+    spdlog::info("--- 正在更新 {} 个可移动实体 ---", view.size_hint());
+
+    // 2. 遍历视图并更新组件(这是最简单的写法，其它写法可参见 https://github.com/skypjack/entt/wiki/Entity-Component-System#views)
+    for (auto entity : view) {
+        auto& pos = view.get<position>(entity);
+        const auto& vel = view.get<velocity>(entity);
+        pos.x += vel.dx;
+        pos.y += vel.dy;
     }
-    if (!registry.valid(enemy)) {
-        spdlog::info("敌人实体已失效。");
-    }
+
+    spdlog::info("=== 游戏 update 结束后 ===");
+    auto final_player_pos = registry.get<position>(player);
+    spdlog::info("玩家更新后位置: ({}, {})", final_player_pos.x, final_player_pos.y);
+    auto final_enemy_pos = registry.get<position>(enemy);
+    spdlog::info("敌人更新后位置: ({}, {})", final_enemy_pos.x, final_enemy_pos.y);
 
     return 0;
 }
