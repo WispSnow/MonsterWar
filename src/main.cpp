bc445b5729280e4380b19d3f1aaabad5be7d43e3
Ziyu <theorhythm@gmail.com>
2025-08-24 11:49:37 +0800

    配置

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,51 @@
 #include <imgui_impl_sdl3.h>
 #include <imgui_impl_sdlrenderer3.h>
 
+void ImGuiOptionalSettings() {
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // 设置 ImGui 主题
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // 设置缩放
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());     // 与系统缩放一致
+    // float main_scale = 1.0f;     // 或者直接设置更加稳定
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);        // 固定样式缩放比例。
+    style.FontScaleDpi = main_scale;        // 设置初始字体缩放比例。
+
+    // 设置透明度
+    float window_alpha = 0.5f;
+
+    // 修改各个UI元素的透明度
+    style.Colors[ImGuiCol_WindowBg].w = window_alpha;
+    style.Colors[ImGuiCol_PopupBg].w = window_alpha;
+
+    // 为了正确显示中文，我们需要加载支持中文的字体。
+    ImFont* font = io.Fonts->AddFontFromFileTTF(
+        "assets/fonts/VonwaonBitmap-16px.ttf",  // 字体文件路径
+        16.0f,                                  // 字体大小
+        nullptr,                                // 字体配置参数
+        io.Fonts->GetGlyphRangesChineseSimplifiedCommon() // 字符范围
+    );
+    if (!font)
+    {
+        // 如果字体加载失败，回退到默认字体，但中文将无法显示。
+        io.Fonts->AddFontDefault();
+        spdlog::warn("警告：无法加载中文字体，中文字符将无法正确显示。");
+    }
+}
+
 void ImGuiInit(SDL_Window *window, SDL_Renderer *renderer) {
     // 初始化 ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+
+    // 可选配置
+    ImGuiOptionalSettings();
 
     // 初始化 ImGui 的 SDL3 和 SDL_Renderer3 后端
     ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
