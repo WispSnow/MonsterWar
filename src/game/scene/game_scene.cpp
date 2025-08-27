fc0240f0a4412828f1ef9ea43c82a90fcff721b4
Ziyu <theorhythm@gmail.com>
2025-08-27 10:14:38 +0800

    项目起点

---
+++ src/game/scene/game_scene.cpp
@@ -0,0 +1,12 @@
+#include "game_scene.h"
+
+namespace game::scene {
+
+GameScene::GameScene(engine::core::Context& context, engine::scene::SceneManager& scene_manager)
+    : Scene("GameScene", context, scene_manager) {
+}
+
+GameScene::~GameScene() {
+}
+
+} // namespace game::scene
