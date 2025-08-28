b0739b1ff6ddcee30653eed07152f35e31ab9ff3
Ziyu <theorhythm@gmail.com>
2025-08-28 10:31:30 +0800

    完成输入模块的信号功能

--- src/game/scene/game_scene.h
+++ src/game/scene/game_scene.h
@@ -8,6 +8,14 @@ public:
     GameScene(engine::core::Context& context, engine::scene::SceneManager& scene_manager);
     ~GameScene();
 
+    void init() override;
+    void clean() override;
+
+private:
+    // --- 测试输入回调事件 ---
+    void onAttack();
+    void onJump();
+
 };
 
 } // namespace game::scene
